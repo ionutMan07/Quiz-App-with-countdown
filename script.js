e9298416ca160b8309ebe8ca1df70b4568e3cbb9
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2021-02-06 22:40:33 +0100

    done

--- script.js
+++ script.js
@@ -1,15 +1,134 @@
-const timer = '1 Jan 2022';
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+
+const timer = '27 Jan 2022';
 
 function countdown() {
   const newYearDate = new Date(timer);
   const currentDate = new Date();
 
-  const seconds = (timer - currentDate) * 1000;
-  const days = Math.floor(seconds / 3600 / 24);
+  const totalSeconds = (newYearDate - currentDate) / 1000;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-  console.log(days);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondEl.innerHTML = formatTime(seconds);
 }
-
+function formatTime(time) {
+  return time < 10 ? `0${time}` : time;
+}
 countdown();
 
 setInterval(countdown, 1000);
+
+// quiz
+
+const quizData = [
+  {
+    question: 'What is the most used programming language in 2019?',
+    type: 'radio',
+    a: 'Java',
+    b: 'C',
+    c: 'Python',
+    d: 'JavaScript',
+    correct: 'd',
+  },
+  {
+    question: 'Who is the President of US?',
+    type: 'radio',
+    a: 'Florin Pop',
+    b: 'Donald Trump',
+    c: 'Ivan Saldano',
+    d: 'Mihai Andrei',
+    correct: 'b',
+  },
+  {
+    question: 'What does HTML stand for?',
+    type: 'radio',
+    a: 'Hypertext Markup Language',
+    b: 'Cascading Style Sheet',
+    c: 'Jason Object Notation',
+    d: 'Helicopters Terminals Motorboats Lamborginis',
+    correct: 'a',
+  },
+  {
+    question: 'What year was JavaScript launched?',
+    type: 'radio',
+    a: '1996',
+    b: '1995',
+    c: '1994',
+    d: 'none of the above',
+    correct: 'b',
+  },
+];
+const quiz = document.getElementById('quiz');
+const answerEls = document.querySelectorAll('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let answer = undefined;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => {
+    answerEl.checked = false;
+  });
+}
+
+submitBtn.addEventListener('click', () => {
+  // check to see the answer
+  const answer = getSelected();
+
+  if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
+
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+    }
+  }
+});
